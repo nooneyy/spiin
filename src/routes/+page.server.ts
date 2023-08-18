cf21b8ebab40f69a412ec74137c9e5025bd541e9
Tomas Kolarik <tomaskolarik1@seznam.cz>
2023-08-18 21:20:41 +0200

    Adjust for timezones in display time

--- src/routes/+page.server.ts
+++ src/routes/+page.server.ts
@@ -11,6 +11,18 @@ interface Reward {
 export const load: PageServerLoad = async ({ platform }) => {
 	const data = await platform?.env.API_DB.get('db', { cacheTtl: 180 });
 	const jsonRewards: Reward[] = JSON.parse(data ?? '[]');
+	jsonRewards.map((reward) => {
+		const date = new Date(`${reward.disp_time} GMT+0330`);
+		const formatOptions: Intl.DateTimeFormatOptions = {
+			year: 'numeric',
+			month: 'long',
+			day: 'numeric',
+			hour: 'numeric',
+			minute: 'numeric'
+		};
+		const dateFormatter = new Intl.DateTimeFormat('en-US', formatOptions);
+		reward.disp_time = dateFormatter.format(date); // Calling format() directly
+	});
 
 	return { jsonRewards };
 };
